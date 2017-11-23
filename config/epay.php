7dc8dfe5a76c755a449d82600994c939db5e3c07
Kaluchii <mr.kaluchii@mail.ru>
2017-11-23 22:59:40 +0600

    Prepare to test system pay

--- config/epay.php
+++ config/epay.php
@@ -11,17 +11,17 @@ return [
     'PUBLIC_KEY_PATH'           => '../epay_source/kkbca.pem',
     'MERCHANT_ID'               => '92994231',
     // Линк для возврата покупателя в магазин (на сайт) после успешного проведения оплаты
-//    'EPAY_BACK_LINK'            => 'http://saumal.mrkalu0a.beget.tech/thanks',
-    'EPAY_BACK_LINK'            => 'http://saumal/thanks',
+    'EPAY_BACK_LINK'            => 'http://saumal.mrkalu0a.beget.tech/thanks',
+//    'EPAY_BACK_LINK'            => 'http://saumal/thanks',
     // Линк для отправки результата авторизации в магазин.
-//    'EPAY_POST_LINK'            => 'http://saumal.mrkalu0a.beget.tech/order_notice',
-    'EPAY_POST_LINK'            => 'http://saumal/order_notice',
+    'EPAY_POST_LINK'            => 'http://saumal.mrkalu0a.beget.tech/order_notice',
+//    'EPAY_POST_LINK'            => 'http://saumal/order_notice',
     // Линк для отправки неудачного результата авторизации либо информации об ошибке в магазин.
-//    'EPAY_FAILURE_POST_LINK'    => 'http://saumal.mrkalu0a.beget.tech/fail',
-    'EPAY_FAILURE_POST_LINK'    => 'http://saumal/fail',
+    'EPAY_FAILURE_POST_LINK'    => 'http://saumal.mrkalu0a.beget.tech/fail',
+//    'EPAY_FAILURE_POST_LINK'    => 'http://saumal/fail',
     // Линк для возврата покупателя на сайт после безуспешного проведения оплаты
-//    'EPAY_FAILURE_BACK_LINK'    => 'http://saumal.mrkalu0a.beget.tech/fail',
-    'EPAY_FAILURE_BACK_LINK'    => 'http://saumal/fail',
+    'EPAY_FAILURE_BACK_LINK'    => 'http://saumal.mrkalu0a.beget.tech/fail',
+//    'EPAY_FAILURE_BACK_LINK'    => 'http://saumal/fail',
 
     'EPAY_FORM_TEMPLATE'        => 'default.xsl',
 ];
