21c2b3c086be91d41387adb4b843e27a21a614f7
Kaluchii <mr.kaluchii@mail.ru>
2018-01-09 23:41:00 +0600

    Change addr

--- resources/views/front/goPay/goPay.blade.php
+++ resources/views/front/goPay/goPay.blade.php
@@ -3,7 +3,7 @@
     <!-- page -->
     <div class="page">
         <div class="page-content">
-            <form action="https://testpay.kkb.kz/jsp/process/logon.jsp" method="post">
+            <form action="https://epay.kkb.kz/jsp/process/logon.jsp" method="post">
                 @foreach($fields as $key=>$value)
                     <input type="text" hidden name="{{$key}}" value="{{$value}}">
                 @endforeach
